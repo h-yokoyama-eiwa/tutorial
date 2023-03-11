6a736eaa7f9b862d7653fbacb71fe319d2bf7bce
h-yokoyama-eiwa <h-yokoyama@esm.co.jp>
2023-03-11 18:42:52 +0900

    検索機能を表示できるようにした

--- web_application_2/src/components/Search.vue
+++ web_application_2/src/components/Search.vue
@@ -1,7 +1,24 @@
 <template>
-  <div>
-   検索するよ
-  </div>
+  <v-container fluid>
+    <v-row>
+      <v-col>
+        <v-text-field
+          clearable
+          label="書籍名検索"
+        ></v-text-field>
+      </v-col>
+      <v-col>
+        <v-select
+          clearable
+          label="ジャンル検索"
+          :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
+        ></v-select>
+      </v-col>
+      <v-col>
+        <v-btn>検索</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
