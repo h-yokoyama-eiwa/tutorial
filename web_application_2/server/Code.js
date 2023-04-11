868eaad321021ab8cc7108db6c80936c01b59242
h-yokoyama-eiwa <h-yokoyama@esm.co.jp>
2023-04-11 20:44:37 +0900

    DBに値を登録する関数を作成

--- web_application_2/server/Code.js
+++ web_application_2/server/Code.js
@@ -1,5 +1,7 @@
 import doGet from './functions/doGet'
-import sample from './functions/sample'
+// import sample from './functions/sample'
+import updateDb from './functions/updateDb'
 
 global.doGet = doGet
-global.sample = sample
+// global.sample = sample
+global.updateDb = updateDb
