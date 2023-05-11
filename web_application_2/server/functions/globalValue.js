85ae97efedd02dfaeb8da6965b4944584845bbff
h-yokoyama-eiwa <h-yokoyama@esm.co.jp>
2023-05-11 20:14:18 +0900

    globalValueをgitignore

--- web_application_2/server/functions/globalValue.js
+++
@@ -1,7 +0,0 @@
-  // GPCの接続情報
-  export const GPC_CONNECTION_NAME = 'esm-gcp-study:us-central1:modern-study'; //Instance_connection_name
-  export const GPC_USER_NAME = 'esm'; //user_name
-  export const GPC_PASSWORD = 'esm'; //'user_password'
-  export const GPC_DATABASE_NAME = 'web_book_h-yokoyama-eiwa'; //database_name
-  export const GPC_URL = 'jdbc:google:mysql://' + GPC_CONNECTION_NAME + '/' + GPC_DATABASE_NAME;
-  
