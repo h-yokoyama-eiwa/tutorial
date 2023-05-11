95f9d981c89fed5c27e662696e88fd051e78f76f
h-yokoyama-eiwa <h-yokoyama@esm.co.jp>
2023-05-11 19:57:35 +0900

    DBアクセス情報を別切り出ししてgitignoreする

--- web_application_2/server/functions/updateBooksTable.js
+++ web_application_2/server/functions/updateBooksTable.js
@@ -3,12 +3,6 @@
  * @param {object} book
  */
 export default function updateBooksTable (book) {
-  // GPCの接続情報
-  const GPC_CONNECTION_NAME = 'esm-gcp-study:us-central1:modern-study'; //Instance_connection_name
-  const GPC_USER_NAME = 'esm'; //user_name
-  const GPC_PASSWORD = 'esm'; //'user_password'
-  const GPC_DATABASE_NAME = 'web_book_h-yokoyama-eiwa'; //database_name
-  const GPC_URL = 'jdbc:google:mysql://' + GPC_CONNECTION_NAME + '/' + GPC_DATABASE_NAME;
   const gpcConnection = Jdbc.getCloudSqlConnection(GPC_URL, GPC_USER_NAME, GPC_PASSWORD);
 
   // レコード更新用SQL
