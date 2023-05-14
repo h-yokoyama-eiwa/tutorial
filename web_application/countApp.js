60fd9ab6626aa99793dd9db9a2b60a127b1e2e8e
h-yokoyama-eiwa <h-yokoyama@esm.co.jp>
2023-05-14 15:15:45 +0900

    DBアクセス情報を別ファイルに切り出し

--- web_application/countApp.js
+++ web_application/countApp.js
@@ -1,22 +1,3 @@
-// スプレッドシートのIDです。
-const SPREAD_SHEET_ID = '1n672n5k3wzB11tifyqdI2DtwUKsiTdlnwX1GARHZNYk'
-
-// GPCの接続情報
-// GPCのデータベース接続名です。
-const GPC_CONNECTION_NAME = 'esm-gcp-study:us-central1:modern-study'; //Instance_connection_name
-
-// GPCのユーザー名です。
-const GPC_USER_NAME = 'esm'; //user_name
-
-// GPCのパスワードです。
-const GPC_PASSWORD = 'esm'; //'user_password'
-
-// GPCのデータベース名です。
-const GPC_DATABASE_NAME = 'web_counter_h-yokoyama-eiwa'; //database_name
-
-// GPCのデータベース接続URLです。
-const GPC_URL = 'jdbc:google:mysql://' + GPC_CONNECTION_NAME + '/' + GPC_DATABASE_NAME;
-
 // main関数
 function doGet() {
 	// ブラウザへ表示するHTMLのテンプレートです。
