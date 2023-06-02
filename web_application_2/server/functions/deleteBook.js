7e4438fe0b412b8be02841abc592def7494d3898
h-yokoyama-eiwa <h-yokoyama@esm.co.jp>
2023-06-02 19:26:59 +0900

    DBからレコードを削除する関数を作成

---
+++ web_application_2/server/functions/deleteBook.js
@@ -0,0 +1,17 @@
+/**
+ * DBから書籍情報を削除する。
+ * @param {object} book
+ */
+export default function deleteBook (book) {
+	const gpcConnection = Jdbc.getCloudSqlConnection(GPC_URL, GPC_USER_NAME, GPC_PASSWORD);
+	const gpcStatement = gpcConnection.prepareStatement('DELETE FROM books WHERE id=?');
+
+  gpcStatement.setInt(1, book.id);
+  const sqlResults = gpcStatement.executeUpdate();
+
+  // デバッグ用ログ出力
+  Logger.log(sqlResults);
+
+  gpcStatement.close();
+  gpcConnection.close();
+}
